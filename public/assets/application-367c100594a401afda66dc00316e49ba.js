aafcbd73e784d9d9e8e8d75da339f32bb689bffa
TREY <rkh.mymail@gmail.com>
2015-01-12 00:44:03 -0500

    styling

--- public/assets/application-367c100594a401afda66dc00316e49ba.js
+++ public/assets/application-367c100594a401afda66dc00316e49ba.js
@@ -11602,6 +11602,14 @@ return jQuery;
 
 
 }).call(this);
+(function() {
+
+
+}).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
